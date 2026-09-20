066275b7b583e8fd14ac3bfa14036684d8254452
Kz25n2 <kz25n2@gmail.com>
2026-09-21 00:27:40 +0900

    PrismaとSQLiteの導入準備

--- app/components/HomeLink.tsx
+++ app/components/HomeLink.tsx
@@ -4,7 +4,7 @@ export default function homeLink() {
    return(
         <Link
             href="/"
-            className="cursor-pointer mt-4 inline-block"
+            className="cursor-pointer mt-4 inline-block underline hover:text-blue-500"
         >
             ←ホームに戻る
         </Link>
