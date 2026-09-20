1ab15cb265330bbdd663a4fdf5c2a7a0125b9eed
Kz25n2 <kz25n2@gmail.com>
2026-09-21 00:09:54 +0900

    update: 場所登録フォームに訪問日追加

--- app/places/new/page.tsx
+++ app/places/new/page.tsx
@@ -8,15 +8,35 @@ export default function newPlace() {
             <form>
                 <div>
                     <label htmlFor="name">場所の名前</label>
-                    <input type="text" id="name" name="name" />
+                    <input 
+                        type="text"
+                        id="name"
+                        name="name"
+                        required
+                    />
                 </div>
                 <div>
                     <label htmlFor="prefecture">都道府県</label>
-                    <input type="text" id="prefecture" name="prefecture" />
+                    <input 
+                        type="text"
+                        id="prefecture"
+                        name="prefecture"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="visitedAt">訪問日</label>
+                    <input 
+                        type="date"
+                        id="visitedAt"
+                        name="visitedAt"
+                    />
                 </div>
                 <div>
                     <label htmlFor="memo">メモ</label>
-                    <textarea id="memo" name="memo" />
+                    <textarea 
+                        id="memo"
+                        name="memo"
+                />
                 </div>
                 <div>
                     <HomeLink />
